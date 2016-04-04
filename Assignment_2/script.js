40e8aa48899ee7cb175f802426010fe52f8c631e
eichners <eichnersara@gmail.com>
2016-04-04 13:42:26 -0400

    Assignment 2 uploaded with error unresolved
    line 44 of script.js is an anonymous function preventing selector from
    working. Not sure how to resolve

---
+++ Assignment_2/script.js
@@ -0,0 +1,58 @@
+
+//'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png'
+
+
+$(document).ready(function () {
+  var map = L.map('map').setView([40.731649,-73.977814], 10);
+  
+	L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+	   attribution: 'Map tiles by <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	    maxZoom: 18
+	}).addTo(map);
+	 var oddLotsData;
+  
+  // add sql data from cartodb odd lots file	
+	var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
+	    q: 'SELECT * FROM bk_oddlots ORDER BY address DESC LIMIT 500',
+	    format: 'GeoJSON'
+	});
+	     $.getJSON(url)
+	     .done(function (data) {
+	     var oddlotsLayer = data
+	     var lotStyle = {color: '#ff4000',
+	     				fillColor : "#ff4000",
+	         			 weight: 3,
+        				 opacity: .9
+         };
+
+		    var lotClick = function (feature, layer) {
+			layer.bindPopup("<strong>" + feature.properties.address + "</strong>" + "," + "<br/>" + feature.properties.ownername );
+		    }
+		    
+		    oddLotsData = L.geoJson(oddlotsLayer, {
+		    	style: lotStyle,
+		    	onEachFeature: lotClick
+		    }).addTo(map); 
+	      
+	      map.fitBounds(oddLotsData.getBounds());
+
+	$('.LIMIT').change(function () {
+    var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
+      q: 'SELECT * FROM bk_oddlots ORDER BY ownertype DESC LIMIT ' + $(this).val(),
+      format: 'GeoJSON'
+    });
+    $.getJSON(url)
+
+// error with data loading above with anonymous function
+      .done(function (data) {
+      	console.log(data);
+      	var oddlotsLayer = data
+ 
+        oddLotsData.clearLayers();
+        oddLotsData.addData(data);
+
+
+	  });
+	});
+});
+	     });
